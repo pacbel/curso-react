850827f3f8ab9a4709cf58289ccbfcbef232e0d3
Pacbel <carlos.pacheco@pacbel.com.br>
2024-01-04 20:17:29 -0300

    Estudo-Component-React

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Initial from './components/Initial'
+import Logo from './components/logo'
+
+function App() {
+
+
+  return (
+    <>
+      <Logo/>
+      <Initial/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/logo.jsx
@@ -0,0 +1,21 @@
+
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import './Initial.css'
+
+const Logo = () => {
+    return (
+        <div>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Logo
